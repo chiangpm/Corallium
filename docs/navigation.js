02edae852b186989253890dc29e6612d546d2b57
chiangpm <chiangpokman@gmail.com>
2026-06-12 10:16:56 +0800

    Add dark mode

--- docs/navigation.js
+++ docs/navigation.js
@@ -5,6 +5,7 @@ const membersPage = document.getElementById("page_members");
 const homePage = document.getElementById("page_home");
 const placeholder1Page = document.getElementById("page_p1");
 const placeholder2Page = document.getElementById("page_p2");
+// const lightmodeButton = document.getElementById("lightmodebutton")
 
 function navclick_mission() {
     missionPage.style.display = "block";
@@ -45,3 +46,27 @@ function navclick_p2() {
     placeholder1Page.style.display = "none";
     placeholder2Page.style.display = "block";
 }
+
+// let isLightMode = true;
+// function lightmodetoggle() {
+//     let currentTheme = document.documentElement.getAttribute('data-theme');
+  
+//     // If no manual theme is set yet, infer it from system preference
+//     if (!currentTheme) {
+//         currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+//     }
+    
+//     // Calculate and force the next theme state
+//     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+//     document.documentElement.setAttribute('data-theme', newTheme);
+//     localStorage.setItem('theme', newTheme);
+
+
+//     isLightMode = ! isLightMode;
+//     if (isLightMode) {
+//         lightmodeButton.innerText = "☼";
+//     } else {
+//         lightmodeButton.innerText = "☽";
+//     }
+// }
