befc315a3fb90a3db2a20b95078a713c0034254a
chiangpm <chiangpokman@gmail.com>
2026-07-01 03:02:29 +0800

    Update home page and members page

---
+++ docs/membersPage/membersdata.js
@@ -0,0 +1,50 @@
+const membersData = [
+    {
+        departmentName: "Tank Maintenance Department",
+        departmentMembers: [
+            "CHUI Wo Ronald",
+            "LI Shing Hing Cathy",
+            "KWOK Wing Chi Gigi",
+            "OUYANG Yuesheng Kevin",
+            "WANG Zuncheng Marco"
+        ],
+    },
+    {
+        departmentName: "Public Relations Department",
+        departmentMembers: [
+            "CHAK Tin Long Louis",
+            "HUI Yik Ling Simona",
+            "CHAN Man Yung Jaslyn",
+            "CHEN Chiu Tung Coco",
+            "TANG Chuck Fung Tristan",
+            "LEUNG Chin Ying Ariana"
+        ],
+    },
+    {
+        departmentName: "Accounting and Finance Department",
+        departmentMembers: [
+            "Accounting and Finance Team",
+            "FONG Wai Yan Jade",
+            "FU Ho Ka Bobby",
+            "OUYANG Yuesheng Kevin"
+        ]
+    },
+    {
+        departmentName: "Art and Design Department",
+        departmentMembers: [
+            "NGO Chin Wun Eunice",
+            "CHAN Nok Yee Agnes",
+            "NG Hin Yan Rebecca",
+            "LI Sicheng Edwina",
+            "LEUNG Chin Ying Ariana",
+            "ZHOU Mo"
+        ]
+    },
+    {
+        departmentName: "Web Development Department",
+        departmentMembers: [
+            "DENG Weilin William",
+            "OUYANG Yuesheng Kevin"
+        ]
+    }
+];
--- docs/navigation.js
+++ docs/navigation.js
@@ -3,48 +3,67 @@
 const missionPage = document.getElementById("page_mission");
 const membersPage = document.getElementById("page_members");
 const homePage = document.getElementById("page_home");
-const placeholder1Page = document.getElementById("page_p1");
-const placeholder2Page = document.getElementById("page_p2");
+const recruitmentPage = document.getElementById("page_recruitment");
+const newsPage = document.getElementById("page_news");
 // const lightmodeButton = document.getElementById("lightmodebutton")
 
-function navclick_mission() {
+document.addEventListener("DOMContentLoaded", function() {
+    const getMembersFormattedHTML = function() {
+        let htmlContent = "";
+        for (let departmentID = 0; departmentID < membersData.length; departmentID++) {
+            htmlContent += "<div class=\"departmentmemberslist\">";
+            htmlContent += "<h2>" + String(membersData[departmentID].departmentName) + "</h2>";
+            for (let memberID = 0; memberID < (membersData[departmentID].departmentMembers).length; memberID++) {
+                htmlContent += "<p>" + String(membersData[departmentID].departmentMembers[memberID]) + "</p>";
+            }
+            htmlContent += "</div>";
+        }
+        // console.log(htmlContent)
+        return htmlContent;
+    }
+
+    document.getElementById("departments_frame").innerHTML = getMembersFormattedHTML();
+});
+
+
+const navclick_mission = function() {
     missionPage.style.display = "block";
     membersPage.style.display = "none";
     homePage.style.display = "none";
-    placeholder1Page.style.display = "none";
-    placeholder2Page.style.display = "none";
+    recruitmentPage.style.display = "none";
+    newsPage.style.display = "none";
 }
 
-function navclick_members() {
+const navclick_members = function() {
     missionPage.style.display = "none";
     membersPage.style.display = "block";
     homePage.style.display = "none";
-    placeholder1Page.style.display = "none";
-    placeholder2Page.style.display = "none";
+    recruitmentPage.style.display = "none";
+    newsPage.style.display = "none";
 }
 
-function navclick_home() {
+const navclick_home = function() {
     missionPage.style.display = "none";
     membersPage.style.display = "none";
     homePage.style.display = "block";
-    placeholder1Page.style.display = "none";
-    placeholder2Page.style.display = "none";
+    recruitmentPage.style.display = "none";
+    newsPage.style.display = "none";
 }
 
-function navclick_p1() {
+const navclick_recruitment = function() {
     missionPage.style.display = "none";
     membersPage.style.display = "none";
     homePage.style.display = "none";
-    placeholder1Page.style.display = "block";
-    placeholder2Page.style.display = "none";
+    recruitmentPage.style.display = "block";
+    newsPage.style.display = "none";
 }
 
-function navclick_p2() {
+const navclick_news = function() {
     missionPage.style.display = "none";
     membersPage.style.display = "none";
     homePage.style.display = "none";
-    placeholder1Page.style.display = "none";
-    placeholder2Page.style.display = "block";
+    recruitmentPage.style.display = "none";
+    newsPage.style.display = "block";
 }
 
 // let isLightMode = true;
